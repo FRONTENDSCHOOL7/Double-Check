29202893126606fac855462d2d24db38d916e2ee
eunjin.Park <ok94777@naver.com>
2023-10-23 02:04:20 +0900

    feat: 도서 검색 기능 추가 #5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "./styles/GlobalStyle";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+);
