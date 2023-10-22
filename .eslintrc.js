775f8fe73009be929855120f64fb7ea1903f24d1
eunjin.Park <ok94777@naver.com>
2023-10-23 02:04:20 +0900

    feat: 도서 검색 기능 추가 #5

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+};
---
+++ src/Search.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Test2() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchBookList, setSearchBookList] = useState([]);
+  const [bookInfoDetail, setBookInfoDetail] = useState([]);
+
+  const serverURL = "http://localhost:3020"; // 서버의 엔드포인트 URL
+
+  const getSearchBook = async () => {
+    if (!searchBookList) {
+      alert("검색어를 입력해주세요.");
+    }
+
+    try {
+      const response = await axios.get(
+        `${serverURL}/search?searchQuery=${encodeURIComponent(searchQuery)}`,
+      );
+      console.log(response.data); // 서버에서 받은 데이터 출력
+      setSearchBookList(response.data.items);
+    } catch (error) {
+      console.error("Error fetching data from server:", error);
+    }
+  };
+
+  const getBookInfoDetail = async (isbn13) => {
+    try {
+      const res = await axios.get(`${serverURL}/search/book/detail/${isbn13}`);
+      console.log(res.data.items);
+      setBookInfoDetail(res.data.items); // Updated to an array
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={getSearchBook}>Search</button>
+      <ul>
+        {searchBookList &&
+          searchBookList.map((item, index) => (
+            <li key={index}>
+              <button onClick={() => getBookInfoDetail(item.isbn)}>
+                <img src={item.image} />
+                <h3>{item.title}</h3>
+                <span>{item.author}</span>
+              </button>
+            </li>
+          ))}
+      </ul>
+      <article>
+        {bookInfoDetail[0] && (
+          <div>
+            <img src={bookInfoDetail[0].image} alt="책 표지 이미지" />
+            <h2>{bookInfoDetail[0].title}</h2>
+            <span>{bookInfoDetail[0].author}</span>
+            <p>{bookInfoDetail[0].description}</p>
+          </div>
+        )}
+      </article>
+    </div>
+  );
+}
+
+export default Test2;
