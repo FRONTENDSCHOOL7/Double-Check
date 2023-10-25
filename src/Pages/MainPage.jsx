ca78db611aa10463f563d0cf10d59c3022168efe
hsprak9781 <hspark9781@gmail.com>
2023-10-25 10:31:25 +0900

    feat: 로그인, 로그아웃 기능 구현(handleLogin 수정 미완료)

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,5 +1,25 @@
+/* eslint-disable no-unused-vars */
+import Button from 'Components/Common/Button/Button';
 import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { loginCheck } from 'Recoil/LoginCheck';
+import loginToken from 'Recoil/LoginToken';
 
 export default function MainPage() {
-  return <div>MainPage</div>;
+  const [isLogin, setIsLogin] = useRecoilState(loginCheck);
+  const [token, setToken] = useRecoilState(loginToken);
+  console.log(isLogin);
+  console.log(token);
+
+  const logOut = () => {
+    setIsLogin(false);
+    setToken(null);
+  };
+
+  return (
+    <>
+      <h2>MainPage</h2>
+      <button onClick={logOut}>로그아웃</button>
+    </>
+  );
 }
