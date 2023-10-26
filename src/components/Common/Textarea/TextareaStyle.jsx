cd704648d1fb6c9def1eb75a5675e7e271dfa49c
eunjin.Park <ok94777@naver.com>
2023-10-26 10:08:35 +0900

    feat: 텍스트에어리어 스타일 수정

--- src/components/Common/Textarea/TextareaStyle.jsx
+++ src/components/Common/Textarea/TextareaStyle.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const StyledTextarea = styled.textarea`
-  height: ${({ text }) => `${20 * (text.split('\n').length + 1)}px`};
   overflow: hidden;
   border: none;
   padding: 10px;
