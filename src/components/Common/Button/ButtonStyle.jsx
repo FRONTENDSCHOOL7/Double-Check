e8e23dbee38bcaa0464e0cd2b6592d93f518a98d
Hayeon LEE <fhdodl@naver.com>
2023-10-27 00:51:36 +0900

    feat : 버튼 전역 스타일 추가

--- src/components/Common/Button/ButtonStyle.jsx
+++ src/components/Common/Button/ButtonStyle.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   margin: 0;
@@ -27,27 +27,34 @@ const Button = styled.button`
 
 const StyledButton = styled(Button)`
   ${(props) => {
-    if (props.shape === "primary") {
+    if (props.shape === 'primary') {
       return css`
         font-size: var(--font-sm-size);
         font-weight: 400;
         padding: 8px 14px;
         border-radius: 25px;
       `;
-    } else if (props.shape === "sub") {
+    } else if (props.shape === 'sub') {
       return css`
         font-size: var(--font-sm-size);
         font-weight: 400;
         padding: 10px 25px;
         border-radius: 25px;
       `;
-    } else if (props.shape === "big") {
+    } else if (props.shape === 'big') {
       return css`
         font-size: var(--font-sm-size);
         font-weight: 500;
         width: 298px;
         padding: 12px 40px;
         border-radius: 10px;
+      `;
+    } else if (props.shape === 'xsmall') {
+      return css`
+        font-size: var(--font-xxs-size);
+        font-weight: 300;
+        width: 80px;
+        border-radius: 30px;
       `;
     }
   }}
@@ -59,7 +66,7 @@ const WhiteButton = styled(Button)`
   width: 312px;
   padding: 15px 120px;
   border-radius: 8px;
-  color: ${(props) => (props.danger ? "var(--danger-color)" : "var(--black)")};
+  color: ${(props) => (props.danger ? 'var(--danger-color)' : 'var(--black)')};
   background-color: var(--white);
   margin: 2px 0;
 `;
