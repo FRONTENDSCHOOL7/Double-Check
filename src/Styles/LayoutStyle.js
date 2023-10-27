b52784730ba996d90ee4f275f45c6bfa772f05f2
Hayeon LEE <fhdodl@naver.com>
2023-10-27 13:26:11 +0900

    style : 전체 레이아웃 설정

---
+++ src/Styles/LayoutStyle.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const LayoutStyle = styled.div`
+  margin: 0 auto;
+  width: 390px;
+  height: 100vh;
+  background-color: white;
+  padding-top: 70px;
+  padding-bottom: 90px;
+  overflow: scroll;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export default LayoutStyle;
