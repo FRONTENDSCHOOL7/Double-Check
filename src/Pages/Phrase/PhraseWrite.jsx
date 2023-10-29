2d61415e2a32a13b9099b3fb48e65a8d322d76e5
eunjin.Park <ok94777@naver.com>
2023-10-29 20:57:16 +0900

    feat: 글귀 목록 페이지 구현 #40

---
+++ src/Pages/Phrase/PhraseWrite.jsx
@@ -0,0 +1,8 @@
+import EditPhrase from 'components/EditPhrase/EditPhrase';
+import React from 'react';
+
+function PhraseWritePage() {
+  return <EditPhrase />;
+}
+
+export default PhraseWritePage;
