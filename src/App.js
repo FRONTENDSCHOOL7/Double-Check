b616cac04eb2b1e5a2e49b8191e49b7b89c011ee
Hayeon LEE <fhdodl@naver.com>
2023-10-31 03:59:20 +0900

    feat : 글귀목록 버튼 추가

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import LayoutStyle from 'Styles/LayoutStyle';
 // import SplashPage from 'Pages/SplashPage';
 // import ErrorPage from 'Pages/ErrorPage';
 // import BottomBar from 'components/Common/BottomBar';
-// import MainPage from 'Pages/MainPage';
+import MainPage from 'Pages/MainPage';
 import BookRoutes from 'Route/BookRoutes';
 import SearchPage from 'Pages/SearchPage';
 import SignupPage from 'Pages/SignupPage';
 import LoginPage from 'Pages/LoginPage';
-import PhraseWrite from 'Pages/PhrasePage/PhraseWrite';
-import PhraseList from 'Pages/PhrasePage/PhraseList';
+import PhraseList from 'Pages/Phrase/PhraseList';
+import PhraseWritePage from 'Pages/Phrase/PhraseWrite';
 
 // import TestPage from 'Pages/TestPage';
 import NavBar from 'components/Common/NavBar/NavBar';
 
 import { QueryClient } from 'react-query';
 import { QueryClientProvider } from 'react-query';
+import OthersPage from 'Pages/ OthersPage';
 
 function App() {
   const queryClient = new QueryClient({
@@ -32,30 +33,27 @@ function App() {
   });
 
   return (
-
-
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <GlobalStyles />
         {/*  라우터에 영향을 받지않는 컴포넌트들*/}
         <LayoutStyle>
-    
           <Routes>
             {/* <Route path='/*' element={<ErrorPage />} /> */}
             {/* <Route path='/' element={<SplashPage />} /> */}
-            {/* <Route path='/' element={<MainPage />} /> */}
+            <Route path='/' element={<MainPage />} />
             <Route path='/book/*' element={<BookRoutes />} />
             <Route path='/loginpage' element={<LoginPage />} />
             <Route path='/signupPage' element={<SignupPage />} />
-            <Route path='/search' element={<SearchPage />} />
-            <Route path='/phrasewrite' element={<PhraseWrite />} />
+            <Route path='/phrasewrite' element={<PhraseWritePage />} />
             <Route path='/phraselist' element={<PhraseList />} />
+            <Route path='/search' element={<SearchPage />} />
+            <Route path='/profile/:accountname' element={<OthersPage />} />
           </Routes>
           <NavBar />
         </LayoutStyle>
       </BrowserRouter>
     </QueryClientProvider>
-
   );
 }
 export default App;
