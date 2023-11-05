fd3d47f07c22544d39de0ae8331fe2b1d73040d1
eunjin.Park <ok94777@naver.com>
2023-11-06 04:40:44 +0900

    fix: 전체 피드 모달 구현 및 댓글 수 적용

---
+++ src/components/Post/PostGallery.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function PostGallery({ posts }) {
+  return (
+    <SGalleryPostList>
+      {posts.map((post) => (
+        <SGalleryPostItem key={post._id}>
+          <Link to={`/post/${post._id}`}>
+            <GalleryImg src={post.image} alt='책 표지 이미지' />
+          </Link>
+        </SGalleryPostItem>
+      ))}
+    </SGalleryPostList>
+  );
+}
+
+const SGalleryPostList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5px;
+  max-width: 390px;
+  margin: auto;
+  padding: 5px;
+`;
+
+const SGalleryPostItem = styled.li`
+  list-style-type: none;
+`;
+
+const GalleryImg = styled.img`
+  width: 100%;
+  height: -webkit-fill-available;
+  border: 1px solid var(--gray-300);
+  border-radius: 8px;
+`;
