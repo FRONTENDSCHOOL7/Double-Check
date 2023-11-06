3cc90a8b2cb43dd5174b2698cda1972963e7267d
hsprak9781 <hspark9781@gmail.com>
2023-11-06 10:22:42 +0900

    feat: 페이지마다 로그인유무 체크(메인페이지 제외)

---
+++ src/components/Common/ProtectedRoute/RouteModal.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Modal from '../Modal/Modal';
+
+export default function RouteModal({ token, children }) {
+  console.log(`token : ${token}`);
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!token) {
+      setShowModal(true);
+    } else {
+      setShowModal(false);
+    }
+  }, [token]); // 토큰이 변경될 때만 업데이트
+
+  const goToLogin = () => {
+    navigate('/loginPage');
+  };
+
+  const goToHome = () => {
+    navigate('/');
+  };
+
+  if (!token) {
+    return (
+      <>
+        <Modal
+          content='로그인이 필요한 페이지 입니다.'
+          btnTxt='로그인'
+          isVisible={showModal}
+          onConfirm={() => goToLogin()}
+          onCancel={() => goToHome()}
+        />
+      </>
+    );
+  }
+  return children;
+}
