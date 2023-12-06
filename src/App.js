d2cd2b0a5f58a5e7f538177ab899ea8926c84ace
Hayeon LEE <fhdodl@naver.com>
2023-12-06 23:52:24 +0900

    refactor : lazy 적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyles from 'Styles/GlobalStyle';
 import LayoutStyle from 'Styles/LayoutStyle';
 import SetMyInfo from 'Pages/SetMyInfo';
 import PostPage from 'Pages/Post/PostPage';
 import PostDetailPage from 'Pages/Post/PostDetailPage';
 import PostEditPage from 'Pages/Post/PostEditPage';
-import MainPage from 'Pages/MainPage';
-import BookRoutes from 'Route/BookRoutes';
-import SearchPage from 'Pages/SearchPage';
+// import MainPage from 'Pages/MainPage';
+// import BookRoutes from 'Route/BookRoutes';
+// import SearchPage from 'Pages/SearchPage';
 import SignupPage from 'Pages/SignupPage';
 import LoginPage from 'Pages/LoginPage';
-import PhraseList from 'Pages/Phrase/PhraseList';
-import PostMain from 'Pages/Post/PostMain';
+// import PhraseList from 'Pages/Phrase/PhraseList';
+// import PostMain from 'Pages/Post/PostMain';
 import ProfilePage from 'Pages/ProfilePage';
-// import TestPage from 'Pages/TestPage';
 import NavBar from 'components/Common/NavBar/NavBar';
 import { QueryClient } from 'react-query';
 import { QueryClientProvider } from 'react-query';
@@ -27,11 +26,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { navBar } from './Recoil/Navbar';
 import StartLoginPage from 'Pages/StartLoginPage';
-import UserPost from 'components/Post/UserPost';
 import SplashPage from 'Pages/SplashPage';
 import ErrorPage from 'Pages/ErrorPage';
-import PostDetail from 'components/Post/PostDetail';
 import RouteModal from 'components/Common/ProtectedRoute/RouteModal';
+
+// lazy 적용
+const BookRoutes = lazy(() => import('Route/BookRoutes'));
+const MainPage = lazy(() => import('Pages/MainPage'));
+const SearchPage = lazy(() => import('Pages/SearchPage'));
+const PostMain = lazy(() => import('Pages/Post/PostMain'));
+const PhraseList = lazy(() => import('Pages/Phrase/PhraseList'));
 
 function App() {
   const token = localStorage.getItem('token');
@@ -57,95 +61,97 @@ function App() {
         {/*  라우터에 영향을 받지않는 컴포넌트들*/}
         <GlobalStyles />
         <LayoutStyle>
-          <Routes>
-            <Route path='/*' element={<ErrorPage />} />
-            <Route path='/' element={<SplashPage />} />
-            <Route path='/startloginpage' element={<StartLoginPage />} />
-            <Route path='/main' element={<MainPage />} />
-            <Route path='/book/*' element={<BookRoutes />} />
-            <Route path='/loginpage' element={<LoginPage />} />
-            <Route path='/signupPage' element={<SignupPage />} />
-            <Route
-              path='/search'
-              element={
-                <ProtectedRoute>
-                  <SearchPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/phraseupdate/:id'
-              element={
-                <ProtectedRoute>
-                  <PhraseUpdate />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/phraselist'
-              element={
-                <ProtectedRoute>
-                  <PhraseList />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/phrase/upload'
-              element={
-                <ProtectedRoute>
-                  <PhraseUpload />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/post/upload/'
-              element={
-                <ProtectedRoute>
-                  <PostPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/post/:post_id'
-              element={
-                <ProtectedRoute>
-                  <PostDetailPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/post/:post_id/edit'
-              element={
-                <ProtectedRoute>
-                  <PostEditPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/post'
-              element={
-                <ProtectedRoute>
-                  <PostMain />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/setmyinfo'
-              element={
-                <ProtectedRoute>
-                  <SetMyInfo />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='/profile/:accountname'
-              element={
-                <ProtectedRoute>
-                  <ProfilePage />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path='/*' element={<ErrorPage />} />
+              <Route path='/' element={<SplashPage />} />
+              <Route path='/startloginpage' element={<StartLoginPage />} />
+              <Route path='/main' element={<MainPage />} />
+              <Route path='/book/*' element={<BookRoutes />} />
+              <Route path='/loginpage' element={<LoginPage />} />
+              <Route path='/signupPage' element={<SignupPage />} />
+              <Route
+                path='/search'
+                element={
+                  <ProtectedRoute>
+                    <SearchPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/phraseupdate/:id'
+                element={
+                  <ProtectedRoute>
+                    <PhraseUpdate />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/phraselist'
+                element={
+                  <ProtectedRoute>
+                    <PhraseList />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/phrase/upload'
+                element={
+                  <ProtectedRoute>
+                    <PhraseUpload />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/post/upload/'
+                element={
+                  <ProtectedRoute>
+                    <PostPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/post/:post_id'
+                element={
+                  <ProtectedRoute>
+                    <PostDetailPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/post/:post_id/edit'
+                element={
+                  <ProtectedRoute>
+                    <PostEditPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/post'
+                element={
+                  <ProtectedRoute>
+                    <PostMain />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/setmyinfo'
+                element={
+                  <ProtectedRoute>
+                    <SetMyInfo />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='/profile/:accountname'
+                element={
+                  <ProtectedRoute>
+                    <ProfilePage />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </Suspense>
         </LayoutStyle>
         {showNavBar && <NavBar />}
       </BrowserRouter>
