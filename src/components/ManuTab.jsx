9bdbf8361d6fa0aeffd3ee182b0a0a26d462edfb
Hayeon LEE <fhdodl@naver.com>
2023-12-06 23:53:36 +0900

    feat : 메인화면 탭 버튼 추가

---
+++ src/components/ManuTab.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+const ManuTab = () => {
+  return (
+    <section>
+      <ManuList>
+        <li>
+          <SMaunTabs to='/#'>
+            <SMaunTabBox></SMaunTabBox>
+          </SMaunTabs>
+        </li>
+        <li>
+          <SMaunTabs to='/#'>
+            <SMaunTabBox></SMaunTabBox>
+          </SMaunTabs>
+        </li>
+        <li>
+          <SMaunTabs to='/#'>
+            <SMaunTabBox></SMaunTabBox>
+          </SMaunTabs>
+        </li>
+      </ManuList>
+    </section>
+  );
+};
+const ManuList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 0px 10px;
+
+  justify-items: center;
+`;
+const SMaunTabBox = styled.div`
+  width: 110px;
+  height: 110px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  border-radius: 10px;
+`;
+const SMaunTabs = styled(Link)``;
+export default ManuTab;
