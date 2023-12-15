514306e507b3d99b785d600b1ff34afbe12375d7
Hayeon LEE <fhdodl@naver.com>
2023-12-16 01:37:21 +0900

    refactor : lazy 적용

--- src/Route/BookRoutes.jsx
+++ src/Route/BookRoutes.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import BookListPage from 'Pages/Book/BookListPage';
-import BookDetailPage from 'Pages/Book/BookDetailPage';
-import BookMainPage from 'Pages/Book/BookMainPage';
+const BookListPage = lazy(() => import('Pages/Book/BookListPage'));
+const BookDetailPage = lazy(() => import('Pages/Book/BookDetailPage'));
+const BookMainPage = lazy(() => import('Pages/Book/BookMainPage'));
 export default function BookRoutes() {
   return (
-    <Routes>
-      <Route path='/main' element={<BookMainPage />} />
-      <Route
-        path='bestseller'
-        element={<BookListPage listType='bestseller' title='베스트 셀러' />}
-      />
-      <Route
-        path='newbooks'
-        element={<BookListPage listType='newbooks' title='신간 도서 리스트' />}
-      />
-      <Route
-        path='special'
-        element={<BookListPage listType='special' title='이번달 신간 리스트' />}
-      />
-      <Route path=':isbn' element={<BookDetailPage />} />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path='/main' element={<BookMainPage />} />
+        <Route
+          path='bestseller'
+          element={<BookListPage listType='bestseller' title='베스트 셀러' />}
+        />
+        <Route
+          path='newbooks'
+          element={<BookListPage listType='newbooks' title='신간 도서 리스트' />}
+        />
+        <Route
+          path='special'
+          element={<BookListPage listType='special' title='이번달 신간 리스트' />}
+        />
+        <Route path=':isbn' element={<BookDetailPage />} />
+      </Routes>
+    </Suspense>
   );
 }
