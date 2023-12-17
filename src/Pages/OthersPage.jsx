cac7090a1a2a9f9bbda65b617a722f583ef44171
Hayeon LEE <fhdodl@naver.com>
2023-12-17 23:05:15 +0900

    remove

--- src/Pages/OthersPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-export default function OthersPage() {
-  const { accountname } = useParams();
-
-  console.log(accountname);
-  return <div> OthersPage</div>;
-}
